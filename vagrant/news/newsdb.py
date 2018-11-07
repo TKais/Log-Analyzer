8245287d5e171cefc95e1bf071473f40f4010efb
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-06 19:22:47 -0800

    feat: Add a query method for getting the most popular articles

--- vagrant/news/newsdb.py
+++ vagrant/news/newsdb.py
@@ -4,12 +4,13 @@ DBNAME="news"
 
 def _connect_to_db():
   db = psycopg2.connect(database=DBNAME)
-  cursor = b.cursor();
+  cursor = db.cursor();
   return (db, cursor)
 
-def get_answers():
+def get_most_popular():
   db = _connect_to_db()[0]
   cursor = _connect_to_db()[1]
+
   select_query = "select author from articles"
   cursor.execute(select_query)
   return cursor.fetchall()
