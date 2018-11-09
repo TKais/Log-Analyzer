8589873b34aa2a8195eaa852af4701ab3387e0c4
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-08 20:23:46 -0800

    feat: Add new query to get the correct title

--- vagrant/news/newsdb.py
+++ vagrant/news/newsdb.py
@@ -6,12 +6,7 @@ def get_most_popular():
   print "Are we here?"
   db = psycopg2.connect(database=DBNAME)
   cursor = db.cursor();
-
-  select_query = "SELECT path, count(*) AS num FROM log GROUP BY path ORDER BY num DESC LIMIT 4 OFFSET 1"
-  # 1. ('/article/candidate-is-jerk', 338647L)
-  # 2. ('/article/bears-love-berries', 253801L)
-  # 3. ('/article/bad-things-gone', 170098L)
-  # "SELECT path, count(*) AS num FROM log GROUP BY path ORDER BY num DESC LIMIT 4 OFFSET 1"
+  select_query = "SELECT title, count(*) as total from articles, log WHERE log.path LIKE concat('%', articles.slug) GROUP BY articles.title ORDER BY total DESC LIMIT 3"
   cursor.execute(select_query)
   return cursor.fetchall()
   db.close()
