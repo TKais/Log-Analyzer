698dbda1ba123aa7f0142b3b79f5573b1f7823a8
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-08 20:40:13 -0800

    feat: Return correct results and plot them in the HTML

--- vagrant/news/news.py
+++ vagrant/news/news.py
@@ -1,33 +1,40 @@
 from flask import Flask, request, redirect, url_for
 
-from newsdb import get_most_popular
+from newsdb import get_most_popular_articles
 
 app = Flask(__name__)
 
 HTML = '''\
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Log Analyzer</title>
-    <style>
-      h3, p { text-align: center; }
-      p { margin-bottom: 3rem; }
-      
-    </style>
-  </head>
-  <body>
-    <h1>Log Analyzer</h1>
-    <h3>What are the most popular three articles of all time?</h3>
-    <h3>Who are the most popular article authors of all time?</h3>
-    <h3>On which days did more than 1% of requests lead to errors?</h3>
-  </body>
-</html>
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Log Analyzer</title>
+      <style>
+        h3, p { text-align: center; }
+        p { margin-bottom: 3rem; }
+        
+      </style>
+    </head>
+    <body>
+      <h1>Log Analyzer</h1>
+      <h3>What are the most popular three articles of all time?</h3>
+      <h3>Who are the most popular article authors of all time?</h3>
+      <h3>On which days did more than 1 percent of requests lead to errors?</h3>
+      %s
+    </body>
+  </html>
+'''
+
+ARTICLE = '''\
+  <div class=post><em class=date>%s</em><br>%s</div>
 '''
 
 @app.route('/', methods=['GET'])
 def main():
-  print "Hey"
-  return HTML
+  result = get_most_popular_articles()
+  articles = "".join(ARTICLE % (title, view_count) for title, view_count in result)
+  html_output = HTML % articles
+  return html_output
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
--- vagrant/news/newsdb.py
+++ vagrant/news/newsdb.py
@@ -2,11 +2,10 @@ import psycopg2
 
 DBNAME="news"
 
-def get_most_popular():
-  print "Are we here?"
+def get_most_popular_articles():
   db = psycopg2.connect(database=DBNAME)
   cursor = db.cursor();
-  select_query = "SELECT title, count(*) as total from articles, log WHERE log.path LIKE concat('%', articles.slug) GROUP BY articles.title ORDER BY total DESC LIMIT 3"
+  select_query = "SELECT title, count(*) as view_count from articles, log WHERE log.path LIKE concat('%', articles.slug) GROUP BY articles.title ORDER BY view_count DESC LIMIT 3"
   cursor.execute(select_query)
   return cursor.fetchall()
   db.close()
