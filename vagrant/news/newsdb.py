f62fd2e82853235b4d9a29a72456b52f87fa9536
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-11 20:46:48 -0800

    feat: Create views in a different way

--- vagrant/news/newsdb.py
+++ vagrant/news/newsdb.py
@@ -10,19 +10,17 @@ def get_result(db_query):
   db.close()
   return result
 
-def check_for_view():
-  check_for_view = "select viewname from pg_catalog.pg_views where viewname = 'author_view'";
-  result = get_result(check_for_view)
-  print(result)
-  if len(result) == 0:
-    create_view()
-
-def create_view():
+def create_views():
+  views = {
+    "author_view": "CREATE OR REPLACE VIEW author_view AS SELECT authors.name, articles.title, count(log.path) AS view_count FROM authors, articles, log WHERE authors.id = articles.author and log.path LIKE concat('%', articles.slug) GROUP BY authors.name, articles.title, log.path;",
+    "error_view": "CREATE OR REPLACE VIEW error_view AS SELECT DATE(time), status, count(status) as num from log GROUP BY DATE(time), status;",
+    "sum_view": "CREATE OR REPLACE VIEW sum_view AS SELECT date, SUM(num) from error_view GROUP BY date;",
+  }
   db = psycopg2.connect(database=DBNAME)
   cursor = db.cursor();
-  view = "CREATE VIEW author_view AS SELECT authors.name, articles.title, count(log.path) AS view_count FROM authors, articles, log WHERE authors.id = articles.author and log.path LIKE concat('%', articles.slug) GROUP BY authors.name, articles.title, log.path;"
-  cursor.execute(view)
-  db.commit()
+  for view in views:
+    cursor.execute(views[view])
+    db.commit()
   db.close()
 
 def get_articles():
@@ -36,6 +34,6 @@ def get_authors():
   return authors
 
 def get_errors():
-  select_query = ""
+  select_query = "SELECT * FROM (SELECT date, ROUND(num * 100 / sum, 2) AS percentage FROM error_view, sum_view WHERE date = day AND status = '404 NOT FOUND') AS example GROUP BY date, percentage HAVING percentage > 1;"
   errors = get_result(select_query)
   return errors
