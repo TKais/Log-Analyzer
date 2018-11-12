c361c05ba3109c1cf0f991c30ad4ea428c486234
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-12 11:37:46 -0800

    fix: Format percentages correctly

--- vagrant/news/news.py
+++ vagrant/news/news.py
@@ -4,7 +4,7 @@ from newsdb import get_articles, get_authors, get_errors, check_for_views
 from pathlib import Path
 
 def check_for_output_file():
-  print("\n Checking if text file 'output.txt' exists...")
+  print("\nChecking if text file 'output.txt' exists...")
   output_file = Path("./output.txt")
   if output_file.is_file():
     print("\n File 'output.txt' exists! Here are the results: ")
@@ -33,8 +33,12 @@ def write_to_file(question, results):
   new_file.write("\n \n ===" + question + "\n \n")
   print("\n \n ===" + question + "\n \n")
   for result in results:
-    print("%s : %d views" % (result[0], result[1]))
-    new_file.write("%s -- %d views \n" % (result[0], result[1]))
+    if 'percent' in question:
+      print("%s -- %.2f%% of errors" % (result[0], result[1]))
+      new_file.write("%s -- %.2f%% of errors \n" % (result[0], result[1]))
+    else:
+      print("%s -- %d views" % (result[0], result[1]))
+      new_file.write("%s -- %d views \n" % (result[0], result[1]))
   new_file.close()
 
 def start():
