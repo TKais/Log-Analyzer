65eaa6af6d73041720387be92e15fe01d798df0d
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-12 10:36:57 -0800

    refactor: Refactor database method calls to be less redundant

--- vagrant/news/news.py
+++ vagrant/news/news.py
@@ -3,40 +3,43 @@
 from newsdb import get_articles, get_authors, get_errors, check_for_views
 from pathlib import Path
 
-def get_most_popular_articles():
-  results = get_articles()
-  output_file = Path('./output.txt')
-  question = 'What are the most popular three articles of all time?'
+def check_for_output_file():
+  print("\n Checking if text file 'output.txt' exists...")
+  output_file = Path("./output.txt")
   if output_file.is_file():
-    file = open('output.txt', 'r')
+    print("\n File 'output.txt' exists! Here are the results: ")
+    file = open("output.txt", "r")
     print(file.read())
     file.close()
   else:
-    write_to_file(question, 'w', results)
+    new_file = open("output.txt", "w+")
+    new_file.close()
+    print("Text file 'output.txt' created. Please open this file once script has finished running. \n")
+    run_db_queries()
 
-def get_most_popular_authors():
-  results = get_authors()
-  question = 'Who are the most popular article authors of all time?'
-  write_to_file(question, 'a', results)
+def run_db_queries():
+  print("Running database queries. This may take a moment... \n")
+  queries = {
+    "a - articles": {"question": "What are the most popular three articles of all time?", "results": get_articles()},
+    "b - authors": {"question": "Who are the most popular article authors of all time?", "results": get_authors()},
+    "c - errors": {"question": "On which days did more than 1 percent of requests lead to errors?", "results": get_errors()},
+  }
 
-def get_days_with_errors():
-  results = get_errors()
-  question = "On which days did more than 1 percent of requests lead to errors?"
-  print(results)
+  for query in sorted(queries.keys()):
+    write_to_file(queries[query]["question"], queries[query]["results"])
 
-def write_to_file(question, mode, results):
-  new_file = open('output.txt', mode)
-  new_file.write('\n' + question + '\n \n')
-  print(question + '\n')
+def write_to_file(question, results):
+  new_file = open("output.txt", "a")
+  new_file.write("\n \n ===" + question + "\n \n")
+  print("\n \n ===" + question + "\n \n")
   for result in results:
-    print('%s : %d views' % (result[0], result[1]))
-    new_file.write('%s -- %d views \n' % (result[0], result[1]))
+    print("%s : %d views" % (result[0], result[1]))
+    new_file.write("%s -- %d views \n" % (result[0], result[1]))
   new_file.close()
 
 def start():
   check_for_views()
-  get_most_popular_articles()
-  get_most_popular_authors()
-  get_days_with_errors()
+  check_for_output_file()
+  print("\n \n ***Database queries complete!*** \n")
 
 start()
